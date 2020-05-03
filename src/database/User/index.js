f3289b6df63b0d9ddf8440ea04e849c8b77215c1
Miguel-Coruj <miguelcoruj@gmail.com>
2020-05-03 09:40:19 -0300

    DataBase add User

---
+++ src/database/User/index.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    token: {
+        type: String,
+        select: false,
+    },
+    RecoverPass: {
+        type: String,
+        select: false,
+    },
+    DateRecover: {
+        type: Date,
+        select: false,
+    }
+})
+
+UserSchema.pre('save', async (next) => {
+    const hash = await bcrypt.hash(this.password, 10)
+    this.password = hash
+})
+
+module.exports = mongoose.model('User', UserSchema)

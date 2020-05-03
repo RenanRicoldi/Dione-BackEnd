bd6f3e24058e1104bd308d176653451b5097e6e3
Miguel-Coruj <miguelcoruj@gmail.com>
2020-05-03 19:40:12 -0300

    Route User

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,12 @@
 const {  Router  } = require('express')
 
+const User = require('./controllers/UserController')
+
 const routes = Router()
 
-
+routes.post('/register', User.create)
+routes.post('/login', User.index)
+routes.delete('/delete', User.delete)
+routes.put('/editar', User.put)
 
 module.exports = routes

73dad54be070950c7342dbc92de409e8ffd9bd6e
Miguel-Coruj <miguelcoruj@gmail.com>
2020-05-05 12:13:16 -0300

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 require('dotenv/config')
+require('./database')
 const express = require('express')
 const route = require('./routes')
 
 const app = express()
 
 app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
 
 app.use(route)
 

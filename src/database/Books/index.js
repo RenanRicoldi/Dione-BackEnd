dd9c5c074b643b6e85227cadf327a883594e7546
Miguel-Coruj <miguelcoruj@gmail.com>
2020-05-17 12:26:51 -0300

    Add Books from DataBase

---
+++ src/database/Books/index.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+
+const BookSchema = new mongoose.Schema({
+    Title: {
+        type: String, 
+        required: true
+    },
+    Author: {
+        type: String,
+        required: true
+    },
+    Editor: {
+        type: String,
+        required: true
+    },
+    urlBook: URL,
+    Libray: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Librays'
+    },
+    Tag: {
+        type: String,
+        required: true
+    }
+})
+
+const Book = mongoose.model('Books', BookSchema)
+
+module.exports = Book

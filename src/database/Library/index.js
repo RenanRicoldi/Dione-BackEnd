c31d7a4f62b82c4411a2912285cd6e34ff36614f
Miguel-Coruj <miguelcoruj@gmail.com>
2020-05-30 19:37:15 -0300

    fixing code

--- src/database/Library/index.js
+++ src/database/Library/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const bcrypt = require('bcryptjs')
 
-const LibraySchema = new mongoose.Schema({
+const LibrarySchema = new mongoose.Schema({
     name: {
         type: String, 
         required: true
@@ -30,13 +30,13 @@ const LibraySchema = new mongoose.Schema({
     }
 })
 
-LibraySchema.pre('save', async function(next) {
+LibrarySchema.pre('save', async function(next) {
     const hash = await bcrypt.hash(this.password, 10)
     this.password = hash
 
     next()
 })
 
-const Libray = mongoose.model('Librays', LibraySchema)
+const Libray = mongoose.model('Librays', LibrarySchema)
 
 module.exports = Libray

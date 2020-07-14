77f3dd26e2e953cd7edc52b9d6c92506d8e1cf3b
Miguel-Coruj <miguelcoruj@gmail.com>
2020-07-14 19:45:02 -0300

    Add Library from Routes

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,6 @@ const Library = require('./controllers/Library')
 const routes = Router()
 
 routes.use('/user', User)
-routes.post('/library', Library)
+routes.use('/library', Library)
 
 module.exports = routes
